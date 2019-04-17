834f6fb8f2d2db0184e1404e402253d99d16bf73
Montag55 <lucas.huebner@uni-weimar.de>
2019-04-18 00:42:54 +0200

    added permissions and location

--- app/src/main/java/com/example/misexercuse_2/MapsActivity.java
+++ app/src/main/java/com/example/misexercuse_2/MapsActivity.java
@@ -1,7 +1,16 @@
 package com.example.misexercuse_2;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -12,18 +21,26 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    public static final int MY_PERMISSIONS_REQUEST_LOCATION = 1;
     private GoogleMap mMap;
+    private EditText textInput;
+    LocationManager locationManager;
+    LocationListener locationListener;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-    }
 
+        textInput = (EditText) findViewById(R.id.textInput);
+
+
+    }
 
     /**
      * Manipulates the map once available.
@@ -38,9 +55,87 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        locationManager = (LocationManager) getSystemService(MapsActivity.this.LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
+        mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+            @Override
+            public void onMapLongClick(LatLng point) {
+                mMap.addMarker(new MarkerOptions().position(point).title(textInput.getText().toString()));
+                String lat = String.format("%.02f", point.latitude);
+                String lon = String.format("%.02f", point.longitude);
+                Toast.makeText(MapsActivity.this, "New location:\n" + textInput.getText().toString() + "(" + lat + ", " + lon + ")" , Toast.LENGTH_LONG).show();
+            }
+        });
+
+        checkLocationPermission();
+    }
+
+    public void centreMapOnLocation(Location location, String title) {
+        LatLng coordinate = new LatLng(location.getLatitude(), location.getLongitude());
+        mMap.clear();
+        mMap.addMarker(new MarkerOptions().position(coordinate).title(title));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(coordinate, 15));
+
+        String lat = String.format("%.02f", coordinate.latitude);
+        String lon = String.format("%.02f", coordinate.longitude);
+        Toast.makeText(MapsActivity.this, "Your location:\n" + textInput.getText().toString() + "(" + lat + ", " + lon + ")" , Toast.LENGTH_LONG).show();
+    }
+
+    public void checkLocationPermission() {
+
+        if (ContextCompat.checkSelfPermission(MapsActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 10, locationListener);
+                Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                centreMapOnLocation(lastKnownLocation,"Your Location");
+            } else{
+                Toast.makeText(MapsActivity.this, "System does not support GPS_PROVIDER", Toast.LENGTH_LONG).show();
+            }
+        } else {
+            ActivityCompat.requestPermissions(MapsActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSIONS_REQUEST_LOCATION);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            if (ContextCompat.checkSelfPermission(MapsActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,1000,10, locationListener);
+                    Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                    centreMapOnLocation(lastKnownLocation,"Your Location");
+                } else {
+                    Toast.makeText(MapsActivity.this, "System does not support GPS_PROVIDER", Toast.LENGTH_LONG).show();
+                }
+            } else {
+                Toast.makeText(MapsActivity.this, "Permission not granted", Toast.LENGTH_LONG).show();
+            }
+        } else {
+            Toast.makeText(MapsActivity.this, "Permission to access GPS denied", Toast.LENGTH_LONG).show();
+        }
     }
 }
